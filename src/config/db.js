e1f415de0cb54c8d7bc868f96a76d0ccb6ee93e7
aadhavan04 <aadhavan142@gmail.com>
2026-06-26 12:31:36 +0530

    mongodb fixed

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/b2b_billing";
+  const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/b2b_billing";
 
   try {
     await mongoose.connect(uri);
